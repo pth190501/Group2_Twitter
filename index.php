9a71827954ac1a2794810bbf18b763fcd1b3efb7
Hai <pth190501@gmail.com>
2021-12-05 10:15:23 +0700

    add sign up page display

--- index.php
+++ index.php
@@ -1,63 +1,42 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php include 'back-end/shared/hearder.php'; ?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="front-end/assets/css/styles.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,400&display=swap" rel="stylesheet">
-    <title>Twitter. It's what's happening</title>
-</head>
-
-<body>
-    <section class="container-fluid p-0">
-        <div class="row">
-            <div class="col-lg-8">
-                <img class="img-fluid" src="front-end/assets/img/Back-logo.png" alt="Twitter">
-            </div>
-
-            <div class="col-lg-4">
-                <div class="row pt-5">
-                    <div class="bird-icon">
-                        <i class="fa-brands fa-twitter logo"></i>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="content1">
-                        <span>It's happening right now.</span>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="content2">
-                        <span>Join Twitter today.</span>
-                    </div>
-                </div>
-
-                <div class="d-grid gap-2 col-10 mx-auto p-5">
-                    <a href="signUp.php" class="btn btn-primary" type="button">Sign Up</a>
-                    <a href="logIn.php" class="btn btn-info mt-3" type="button">Log In</a>
-                </div>
-                
-            </div>
+<section class="container-fluid p-0">
+    <div class="row">
+        <div class="col-lg-8">
+            <img class="img-fluid" src="front-end/assets/img/Back-logo.png" alt="Twitter">
         </div>
 
-        <footer>
-            <hr class="mt-5">
-            <div class="text-center text-muted">
-                &copy; 2021 Twitter, Inc.
+        <div class="col-lg-4">
+            <div class="row pt-5">
+                <div class="bird-icon">
+                    <i class="fa-brands fa-twitter logo"></i>
+                </div>
             </div>
-        </footer>
-    </section>
-</body>
 
-</html>
+            <div class="row">
+                <div class="content1">
+                    <span>It's happening right now.</span>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="content2">
+                    <span>Join Twitter today.</span>
+                </div>
+            </div>
+
+            <div class="d-grid gap-2 col-10 mx-auto p-5">
+                <a href="signUp.php" class="btn btn-primary" type="button">Sign Up</a>
+                <a href="logIn.php" class="btn btn-info mt-3" type="button">Log In</a>
+            </div>
+
+        </div>
+    </div>
+
+    <footer>
+        <hr class="mt-5">
+        <div class="text-center text-muted">
+            &copy; 2021 Twitter, Inc.
+        </div>
+    </footer>
+</section>
---
+++ signUp.php
@@ -0,0 +1,6 @@
+<?php 
+    $pageTitle = "SignUp | Twitter";
+?>
+
+<?php include 'back-end/shared/hearder.php'; ?>
+

99f9dda403de29e4b6bd788cb669aa4055832d0f
Hai <pth190501@gmail.com>
2021-12-06 09:43:58 +0700

    fix modal done

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
             </div>
 
             <div class="container mt-3">
-                <button type="button" class="btn btn-primary rounded-pill" data-bs-toggle="modal" data-bs-target="#myModal">
+                <button type="button" class="btn btn-primary rounded-pill" data-bs-toggle="modal" data-bs-target="#signUpModal">
                     Sign up
                 </button>
             </div>
@@ -46,15 +46,15 @@
 
                         <!-- Modal body -->
                         <div class="modal-body">
-                            <h1 class="pb-5">Create your account</h1>
+                            <h1 class="pb-3">Create your account</h1>
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">First name</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="First name">
+                                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="First name">
                             </div>
 
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Last name</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Last name">
+                                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Last name">
                             </div>
 
                             <div class="mb-3">
@@ -64,18 +64,18 @@
 
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Password</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Password">
+                                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Password">
                             </div>
 
                             <div class="mb-3">
                                 <label for="exampleFormControlInput1" class="form-label">Confirm password</label>
-                                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Confirm password">
+                                <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="Confirm password">
                             </div>
                         </div>
 
                         <!-- Modal footer -->
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Sign up</button>
+                            <button type="button" class="btn btn-primary rounded-pill" data-bs-dismiss="modal">Sign up</button>
                         </div>
 
                     </div>
@@ -85,7 +85,7 @@
             <div class="container mt-5">
                 <h6>Already have an account?</h6>
 
-                <button type="button" class="btn btn-info rounded-pill" data-bs-toggle="modal" data-bs-target="#myModal">
+                <button type="button" class="btn btn-info rounded-pill" data-bs-toggle="modal" data-bs-target="#logInModal">
                     Sign in
                 </button>
             </div>
@@ -103,12 +103,21 @@
 
                         <!-- Modal body -->
                         <div class="modal-body">
-                            Modal body..
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1">
+                            </div>
+                            
                         </div>
 
                         <!-- Modal footer -->
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary rounded-pill" data-bs-dismiss="modal">Sign in</button>
                         </div>
 
                     </div>

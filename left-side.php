30aff3a3415ee23d599c95c10b2b8dfc660af381
Kinkouuu <93850268+Kinkouuu@users.noreply.github.com>
2021-12-18 19:29:00 +0700

    update sidebar

---
+++ left-side.php
@@ -0,0 +1,46 @@
+<?php require_once 'head.php'; ?> 
+
+                <div class="bird-icon h-10">
+                    <i class="fa-brands fa-twitter logo"></i>
+                </div>
+            <div class="list-group" id="list-tab" role="tablist">
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="home.php" role="tab">
+                    <img src="assets/img/home.svg" alt="">
+                    Home
+                </a>
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="explore.php" role="tab">
+                    <img src="assets/img/tag.svg" alt="">
+                    Explore
+                </a>
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="notifi.php" role="tab">
+                    <img src="assets/img/notifications.svg" alt="">
+                    Notification
+                </a>
+
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="mess.php" role="tab">
+                    <img src="assets/img/message.svg" alt="">
+                    Messages
+                </a>
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="#" role="tab">
+                    <img src="assets/img/bookmark.svg" alt="">
+                    Bookmarks
+                </a>
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="#" role="tab">
+                    <img src="assets/img/list.svg" alt="">
+                    Lists
+                </a>
+                <a class="list-group-item border-0 text-dark" id="list-home-list" data-bs-toggle="list" href="#" role="tab">
+                    <img src="assets/img/profile.svg" alt="">
+                    Profile
+                </a>
+                <div id="btntweet">
+                    <button type="submit" class="btn btn-primary col-6 rounded-pill mt-4">
+                        Tweet
+                    </button>
+                </div>
+                <a class = "mt-5" href="index.php">
+                    LOG OUT
+                </a>
+            </div>
+    </body>
+</html>
---
+++ right-side.php
@@ -0,0 +1,22 @@
+<?php require_once 'sys/head.php'; ?>
+
+
+                <nav class="navbar navbar-expand-lg navbar-light bg-light ">
+                    <form class="d-flex">
+                        <input class="form-control rounded-pill" type="search" placeholder="Search Twitter" aria-label="Search">
+                        <button class="btn btn-success mx-2 " type="submit">Search</button>
+                    </form>
+                </nav>
+                <div class="list-group">
+                    <a href="#" class="list-group-item list-group-item-action active" aria-current="true">
+                    Trend for you
+                    </a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                    <a href="#" class="list-group-item list-group-item-action">A link item</a>
+                </div>
+</body>
+</html>

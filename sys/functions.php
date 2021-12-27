9740d8ddf0583ab62f9cd4895a673ccb6e741cdb
Pham Thanh Hai <pth190501@gmail.com>
2021-12-27 19:32:36 +0700

    update anti sql injection

--- sys/functions.php
+++ sys/functions.php
@@ -5,4 +5,22 @@ function locdata($var)
         $data = trim(addslashes(htmlspecialchars($var)));
         return $data;
     } // anti sql injection
+    
+function mget($name) {
+    if (isset($_GET[$name])) {
+        return locdata($_GET[$name]);
+    } else {
+        return "";
+    }
+}
+
+//get data from post method
+function mpost($name) {
+    if (isset($_POST[$name])) {
+        return locdata($_POST[$name]);
+    } else {
+        return "";
+    }
+}
+
 ?>

2b311364dbb97ee4769e4175319aa93e7fe71ccf
Kinkouuu <93850268+Kinkouuu@users.noreply.github.com>
2022-01-11 16:42:26 +0700

    reponsive Login

--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-$tit = "Log In";
 require_once 'sys/head.php';
 if (isset($_SESSION['user'])) {
     header("Location: home.php");
@@ -19,33 +18,29 @@ if (isset($_POST['login'])) {
 }
 ?>
 
-<div class="login-form">
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-6 p-0 pt-3">
-            <div class="bird-icon">
-                <a href="index.php">
-                    <i class="fa-brands fa-twitter logo"></i>
-                </a>
-            </div>
-            <h3 class="text-center pt-3">Log in to Twitter</h3>
-            <form class="login-form" method="post">
-                <div class="mb-3 bg-color">
-
-                    <input type="text" class="form-control" name="email" placeholder="Phone, email, or username" required="required">
-                </div>
-                <div class="mb-3 bg-color">
-
-                    <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
-                </div>
-                <button type="submit" name="login" class="btn btn-info btn-lg btn-block mt-3">Log in</button>
-                <div class="text-center pt-3 pb-3">
-                    Don't have an account?
-                    <a href="signup.php">Register Here</a>
-                </div>
-            </form>
+<div class="login-form" style="width: 100%;">
+    <div class="col-md-6 mx-auto p-1 pt-3">
+        <div class="bird-icon">
+            <a href="index.php">
+                <i class="fa-brands fa-twitter logo"></i>
+            </a>
         </div>
-        <div class="col-md-3"></div>
+        <h3 class="text-center pt-3">Log in to Twitter</h3>
+        <form class="login-form" method="post">
+            <div class="mb-3 bg-color">
+
+                <input type="text" class="form-control" name="email" placeholder="Email" required="required">
+            </div>
+            <div class="mb-3 bg-color">
+
+                <input type="password" class="form-control" name="pass" placeholder="Password" required="required">
+            </div>
+            <button type="submit" name="login" class="btn btn-info btn-lg btn-block mt-3">Log in</button>
+            <div class="text-center pt-3 pb-3">
+                Don't have an account?
+                <a href="signup.php">Register Here</a>
+            </div>
+        </form>
     </div>
 </div>
 
